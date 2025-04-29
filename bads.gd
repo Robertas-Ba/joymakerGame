717d91af90e8516a4397862640a632bed979efdc
Robertas <C24736925@mytudublin.ie>
2025-04-29 13:35:20 +0100

    updates to scrips
    added skins to mob and made it possible to heal the mob
    
    made updates to bullets and gun

---
+++ bads.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var health = 3
+var skins = [
+	preload("res://0% mob.png"),
+	preload("res://50% mob.png"),
+	preload("res://100% mob.png")
+]
+
+
+	
+@onready var player = get_node("res://joy.tscn")
+
+#func _physics_process(delta):
+	#var direction = global_position.direction_to(player.global_position)
+	#velocity = direction * 300.0
+	#move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
