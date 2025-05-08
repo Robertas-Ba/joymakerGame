9ce9cf8c09415fd4b4beeda323f1edc056021341
Robertas <C24736925@mytudublin.ie>
2025-05-08 14:20:46 +0100

    Update bads.gd

--- bads.gd
+++ bads.gd
@@ -5,13 +5,6 @@ var normal_skin = preload("res://0% mob.png")
 var damaged_skin = preload("res://50% mob.png")
 var full_skin = preload("res://100% mob.png")
 
-@onready var player = get_node("res://joy.tscn")
-
-#func _physics_process(delta):
-	#var direction = global_position.direction_to(player.global_position)
-	#velocity = direction * 300.0
-	#move_and_slide()
-
 func take_damage():
 	health -= 1
 	if health == 2:
