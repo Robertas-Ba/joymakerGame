8d61c5cfd6fb99f6380ee38a317b21468d355ff6
Robertas <C24736925@mytudublin.ie>
2025-05-08 13:47:35 +0100

    Update bads.gd
    the bads now only have 3 hp

--- bads.gd
+++ bads.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var health = 4
+var health = 3
 var normal_skin = preload("res://0% mob.png")
 var damaged_skin = preload("res://50% mob.png")
 var full_skin = preload("res://100% mob.png")
@@ -14,9 +14,9 @@ var full_skin = preload("res://100% mob.png")
 
 func take_damage():
 	health -= 1
-	if health == 3:
-		$"0_Mob".texture = damaged_skin
 	if health == 2:
+		$"0_Mob".texture = damaged_skin
+	if health == 1:
 		$"0_Mob".texture = full_skin
 		
 	if health == 0:
