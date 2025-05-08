aa8809994706d8c5086ef18dd7a489458562f24f
Robertas <C24736925@mytudublin.ie>
2025-05-08 13:43:41 +0100

    bads updated
    removed bottom rigth bads
    and updated the code bads have 4hp now

--- bads.gd
+++ bads.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var health = 3
+var health = 4
 var normal_skin = preload("res://0% mob.png")
 var damaged_skin = preload("res://50% mob.png")
 var full_skin = preload("res://100% mob.png")
@@ -14,8 +14,10 @@ var full_skin = preload("res://100% mob.png")
 
 func take_damage():
 	health -= 1
-	if health == 2:
+	if health == 3:
 		$"0_Mob".texture = damaged_skin
+	if health == 2:
+		$"0_Mob".texture = full_skin
 		
 	if health == 0:
 		queue_free()
