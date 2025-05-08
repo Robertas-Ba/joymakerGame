04ba9f9da03b570303cbe20434d5267c8562e029
Robertas <C24736925@mytudublin.ie>
2025-05-08 13:40:57 +0100

    bads change skin on damaged

--- bads.gd
+++ bads.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var health = 3
 var normal_skin = preload("res://0% mob.png")
 var damaged_skin = preload("res://50% mob.png")
+var full_skin = preload("res://100% mob.png")
 
 @onready var player = get_node("res://joy.tscn")
 
@@ -13,7 +14,9 @@ var damaged_skin = preload("res://50% mob.png")
 
 func take_damage():
 	health -= 1
-	
+	if health == 2:
+		$"0_Mob".texture = damaged_skin
+		
 	if health == 0:
 		queue_free()
 		emit_signal("enemy_killed")
