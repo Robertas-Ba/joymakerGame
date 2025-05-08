86ff118eb23293abbe65e4d541a1d1af14444b0b
Robertas <C24736925@mytudublin.ie>
2025-05-08 14:27:59 +0100

    Update bads.gd
    made the bads follow the player

--- bads.gd
+++ bads.gd
@@ -5,6 +5,14 @@ var normal_skin = preload("res://0% mob.png")
 var damaged_skin = preload("res://50% mob.png")
 var full_skin = preload("res://100% mob.png")
 
+@onready var target=$"../Player"
+var speed=150
+func _physics_process(delta):
+	var direction=(target.position-position).normalized()
+	velocity=direction * speed
+	look_at(target.position)
+	move_and_slide()
+	
 func take_damage():
 	health -= 1
 	if health == 2:
